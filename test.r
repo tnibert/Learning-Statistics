0a9aed36d14598ee981565469c79946576f22e73
Tim Nibert <docz2a@gmail.com>
2019-05-05 11:43:53 +1000

    modified manual standard deviation of random variable

--- test.r
+++ test.r
@@ -8,5 +8,6 @@ plot(y)
 
 # calculate standard deviation of a random variable manually
 a <- c(7.2, 1.2, 1.8, 2.8, 18, -1.9, -0.1, -1.5, 13.0, 3.2, -1.1, 7.0, 0.5, 3.9, 2.1, 4.1, 6.5)
-sqrt(sum((a-sum(a*(1/17)))^2*(1/17))*(17/16))
+# this works because the probability of each element is 1/17
+sqrt(length(a)/(length(a)-1)*sum((a-sum(a*(1/length(a))))^2*(1/length(a))))
 sd(a)
