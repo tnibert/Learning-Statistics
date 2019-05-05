3fb89c187b16115297e6d3a9eaeca8274c4a31d4
Tim Nibert <docz2a@gmail.com>
2019-05-05 10:52:43 +1000

    manual standard deviation calculation

--- test.r
+++ test.r
@@ -5,3 +5,8 @@ y <- table(x)
 print(y)
 #pdf("test.pdf")
 plot(y)
+
+# calculate standard deviation of a random variable manually
+a <- c(7.2, 1.2, 1.8, 2.8, 18, -1.9, -0.1, -1.5, 13.0, 3.2, -1.1, 7.0, 0.5, 3.9, 2.1, 4.1, 6.5)
+sqrt(sum((a-sum(a*(1/17)))^2*(1/17))*(17/16))
+sd(a)
