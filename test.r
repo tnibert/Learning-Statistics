185be786ace66de7392ad41f32e288ca3b90e6f2
Tim Nibert <docz2a@gmail.com>
2019-05-05 13:32:36 +1000

    added some standard deviation with probabilities

--- test.r
+++ test.r
@@ -1,4 +1,6 @@
 #!/usr/bin/env Rscript
+
+# just gettings use to the environment
 x <- c(5, 3, 9, 1, 2, 3, 4, 1, 3)
 print(x)
 y <- table(x)
@@ -11,3 +13,16 @@ a <- c(7.2, 1.2, 1.8, 2.8, 18, -1.9, -0.1, -1.5, 13.0, 3.2, -1.1, 7.0, 0.5, 3.9,
 # this works because the probability of each element is 1/17
 sqrt(length(a)/(length(a)-1)*sum((a-sum(a*(1/length(a))))^2*(1/length(a))))
 sd(a)
+
+# calculate expectation with probabilities
+Y.val <- c(0, 1, 2, 3, 4, 5, 6)
+P.val <- c(0.10, 0.15, 0.25, 0.10, 0.15, 0.10, 0.15)
+E <- sum(Y.val*P.val)
+print("Expectation: ")
+print(E)
+
+# calculate variance and standard deviation with probabilities, this replicates the above with variable probabilities
+Var <- sum((Y.val-E)^2*P.val)
+print(Var)
+standarddeviation <- sqrt(Var)
+print(standarddeviation)
